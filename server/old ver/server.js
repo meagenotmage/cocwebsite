6abb6bb7d295bffa062b6aa99483296bb3399693
meagenotmage <meagelleinerose.sionosa@wvsu.edu.ph>
2026-06-14 19:39:47 +0800

    fix: backend server wrong file namegit add .git add .

--- server/old ver/server.js
+++ server/old ver/server.js
@@ -48,7 +48,11 @@ const Order = mongoose.model('Order', new mongoose.Schema({
 
 // 3. MIDDLEWARE
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://cocwebsite-rocz.onrender.com', 'https://college-of-communication.vercel.app'],
+    origin: ['http://localhost:5500', 
+            'http://127.0.0.1:5500', 
+            'https://cocwebsite-rocz.onrender.com', 
+            'https://college-of-communication.vercel.app',
+            'http://192.168.137.1:8081'],
     credentials: true
 }));
 app.use(express.json());
